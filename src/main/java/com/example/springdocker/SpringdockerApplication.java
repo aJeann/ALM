36c16644ec8d28622b9bc79603a2a7c5b545fe52
aJeann <axel.jeansson@hotmail.se>
2021-05-28 18:15:35 +0200

    added greeting

--- src/main/java/com/example/springdocker/SpringdockerApplication.java
+++ src/main/java/com/example/springdocker/SpringdockerApplication.java
@@ -2,6 +2,8 @@ package com.example.springdocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringdockerApplication {
@@ -9,5 +11,15 @@ public class SpringdockerApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringdockerApplication.class, args);
     }
+
+    @RestController
+    class GreetingController{
+
+        @GetMapping("/")
+        public String greeting(){
+            return "Välkommen till Axels springboot-sida för ALM-kursen";
+        }
+    }
+
 
 }
